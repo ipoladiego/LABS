2a4283aa4a1d7af792ba73cd0830180379938402
Diego Ramos <diramos54321@gmail.com>
2023-02-03 22:44:21 -0300

    commit

---
+++ lampadas/js/config.js
@@ -0,0 +1,8 @@
+function ligarDesliga(){
+    var imagem = document.getElementsByClassName("lamp").src;
+    var imagem_ligado ="midias/pic_bulboff.gif";
+    var imagem_desligado ="midias/pic_bulbon.gif";
+}
+    if (imagem==imagem_ligado){
+            document
+    }
