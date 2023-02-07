e7868ab9670efa1783c0a5109fa6c922a88f7c52
Diego Ramos <diramos54321@gmail.com>
2023-02-06 23:56:25 -0300

    commit00000

--- lampadas/js/config.js
+++ lampadas/js/config.js
@@ -1,8 +1,13 @@
 function ligarDesliga(){
-    var imagem = document.getElementsByClassName("lamp").src;
+
+    var imagem = document.getElementById("lamp").src;
     var imagem_ligado ="midias/pic_bulboff.gif";
     var imagem_desligado ="midias/pic_bulbon.gif";
-}
-    if (imagem==imagem_ligado){
-            document
+
+    if(imagem == imagem_ligado){
+            document.getElementById("lamp").src = imagem_desligado;
+    }else{
+        document.getElementById("lamp").src = imagem_ligado;
     }
+}
+document.getElementById("lamp").addEventListener("click", ligarDesliga);
